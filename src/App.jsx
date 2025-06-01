be9fcee4c79b4f76709b0839e27e805e5a8bb799
Wilton24 <cbasher24@gmail.com>
2025-06-01 22:29:05 +0800

    getting user's current location with the navigator object

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from 'react';
-
+import { useEffect, useRef, useState } from 'react';
+import {sortPlacesByDistance} from './loc.js'
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
@@ -8,8 +8,20 @@ import logoImg from './assets/logo.png';
 
 function App() {
   const modal = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+      setAvailablePlaces(sortedPlaces);    
+    });
+
+  }, []);
+
+
+  
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -35,7 +47,7 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
-  }
+  };
 
   return (
     <>
@@ -63,7 +75,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText={"Sorting Places by distance..."}
           onSelectPlace={handleSelectPlace}
         />
       </main>
